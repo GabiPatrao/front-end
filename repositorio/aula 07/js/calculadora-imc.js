3be87625aa5241dd5f6e47fae56f5db016ba0e1b
GabiPatrao <lopespatraog@gmail.com>
2024-03-04 11:57:36 -0300

    aula 10 html

--- repositorio/aula 07/js/calculadora-imc.js
+++ repositorio/aula 07/js/calculadora-imc.js
@@ -5,26 +5,42 @@ formulario.addEventListener('submit', function(e){ //a função aguarda o usuár
     e.preventDefault(); //muda o comportamento padrão do formulário que estamos escutando
 
     const inputPeso = e.target.querySelector('#peso')
-    
     const peso = Number(inputPeso.value)
     console.log(inputPeso)
     console.log(peso)
     
-
     const inputAltura = e.target.querySelector('#altura')
-    
     const altura = Number(inputAltura.value)
     console.log(inputAltura)
     console.log(altura)
 
     const imc = calcularImc(peso, altura)
-
     console.log(imc)
-    
+
+
+    const classificacao = tabelaImc(imc)
+    mostrarResultado  (classificacao)
 })
 
-//função para calcular o IMC e retornar o valor do imc com duas casas decimais
 function calcularImc(peso, altura){
-    let imc = (peso / altura **2); //fórrmula do imc
-    return imc.toFixed(2); //toFixed(2) foi usado para formatar o número em //2 casas decimais
-} 
+    let imc = (peso / altura ** 2 );
+    return imc.toFixed(2);
+}
+
+function mostrarResultado(msg){
+    const resultado = document.querySelector("#resultado")
+    resultado.innerHTML = msg
+}
+
+function tabelaImc(imc){
+    const classificacao = ['abaixo do peso', 'peso normal', 'sobrepeso', 'obesidade grau I', 'obesidade grau II', 'obesidade grau III']
+
+    if (imc >= 39.9) return classificacao[5];
+    if (imc >= 34.9) return classificacao[4];
+    if (imc >= 29.9) return classificacao[3];
+    if (imc >= 24.9) return classificacao[2];
+    if (imc >= 18.5) return classificacao[1];
+    if (imc >= 18.5) return classificacao[1];
+    if (imc < 18.5) return classificacao[0];
+
+}
